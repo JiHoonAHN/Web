fd2851f0ba7855c3dd7d5694685c1f971390303b
JiHoonAHN <ahnjh1028@naver.com>
2021-01-18 23:45:59 +0900

    study do it book

--- Web2/ViewController.swift
+++ Web2/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator:UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
 
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
